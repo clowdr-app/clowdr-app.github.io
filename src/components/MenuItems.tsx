30f416fd4f398ef15363406324269342e61f24c4
Benjamin Pierce <bcpierce00t@gmail.com>
2020-12-17 12:44:48 -0500

    Add jobs to top-level menu
    (and change "Solution", which I've never understood, to "Platform")

--- src/components/MenuItems.tsx
+++ src/components/MenuItems.tsx
@@ -6,10 +6,13 @@ export default function MenuItems() {
   return (
     <>
       <Menu.Item as={Link} to="/solution" activeClassName="active">
-        Solution
+        Platform
       </Menu.Item>
       <Menu.Item as={Link} to="/about" activeClassName="active">
         About
+      </Menu.Item>
+      <Menu.Item as={Link} to="/jobs" activeClassName="active">
+        Jobs
       </Menu.Item>
     </>
   )
