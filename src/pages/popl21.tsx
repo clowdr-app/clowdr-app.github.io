b9e36ad6ec08e1a77e0835c8e865be46a55c8439
Benjamin Pierce <bcpierce00t@gmail.com>
2021-01-20 20:21:58 -0500

    Add POPL 21 static page

---
+++ src/pages/popl21.tsx
@@ -0,0 +1,208 @@
+import React from "react"
+import { Container, Header, Item, Segment } from "semantic-ui-react"
+import "semantic-ui-css/semantic.min.css"
+import ResponsiveContainer from "../components/ResponsiveContainer"
+import Title from "../components/Title"
+
+export default function Popl21 () {
+  return (
+    <>
+      <Title title="POPL 2021 Lightning Talks" />
+        <Container text style={{ padding: "4em 0" }}>
+          <p style={{ fontSize: "2em", textAlign: "center" }}>
+            POPL Lightning Talks
+          </p>
+
+          <Header as="h2" style={{ fontSize: "1.7em" }} >
+            Thursday, January 21 
+          </Header>
+
+<p><i>(all times in CET)</i></p>
+
+          <p>
+15:45 - 16:00: <b>Awards ceremony</b>
+{" "} in {" "}
+<a href="https://acm-org.zoom.us/j/91683264924?pwd=bktYNUZ6Y3BFWVNXczM4OGhmSVBjdz09">Plenary room</a>
+
+<ul>
+<li>15:45 - 15:55: Most Influential POPL Paper</li>
+
+<li>15:55 - 16:00: Student Research Competition Results</li>
+</ul>
+
+
+
+16:00 - 17:00: <b>Security</b>
+{" "} in {" "}
+<a href="https://zoom.us/j/97045152430?pwd=YzNDU3hvcTdtZ3R0YmxTWGdhZW9vUT09">POPL-A</a>
+
+<ul>
+
+<li>
+16:00 - 16:10:
+Efficient and Provable Local Capability Revocation using Uninitialized Capabilities
+</li>
+
+<li>
+16:10 - 16:20:
+Mechanized Logical Relations for Termination-Insensitive Noninterference
+</li>
+
+<li>
+16:20 - 16:30:
+Giving Semantics to Program-Counter Labels via Secure Effects
+</li>
+
+<li>
+16:30 - 16:40:
+Automata and Fixpoints for Asynchronous Hyperproperties
+</li>
+
+<li>
+16:40 - 16:50:
+Automatically Eliminating Speculative Leaks from Cryptographic Code with Blade
+</li>
+
+</ul>
+
+
+16:00 - 17:00: <b>Concurrency (Shared Memory)</b>
+{" "} in {" "}
+<a href="https://zoom.us/j/94721542929?pwd=dlhVczRWeTcwWE56dTlCOHFLM09CQT09">POPL-B</a>
+
+<ul>
+
+<li>
+16:00 - 16:10:
+Verifying Observational Robustness Against a C11-style Memory Model
+</li>
+
+<li>
+16:10 - 16:20:
+Provably Space Efficient Parallel Functional ProgrammingDistinguished Paper
+</li>
+
+<li>
+16:20 - 16:30:
+Modeling and Analyzing Evaluation Cost of CUDA Kernels
+</li>
+
+<li>
+16:30 - 16:40:
+Optimal Prediction of Synchronization-Preserving Races
+</li>
+
+<li>
+16:40 - 16:50:
+Taming x86-TSO Persistency
+</li>
+
+</ul>
+
+
+
+
+
+17:00 - 18:00: <b>Keynote Talk</b>
+{" "} in {" "}
+<a href="https://acm-org.zoom.us/j/91683264924?pwd=bktYNUZ6Y3BFWVNXczM4OGhmSVBjdz09">Plenary room</a>
+
+<ul><li>
+<i>Dynamical Systems and Program Analysis</i> <br/>
+James Worrell, University of Oxford
+</li></ul>
+
+
+
+
+18:30 - 19:30: <b>Complexity</b>
+{" "} in {" "}
+<a href="https://zoom.us/j/97045152430?pwd=YzNDU3hvcTdtZ3R0YmxTWGdhZW9vUT09">POPL-A</a>
+
+<ul>
+
+<li>
+18:30 - 18:40:
+Learning the Boundary of Inductive Invariants
+</li>
+
+<li>
+18:40 - 18:50:
+The Fine-Grained and Parallel Complexity of Andersen's Pointer Analysis
+</li>
+
+<li>
+18:50 - 19:00::
+Context-Bounded Verification of Liveness Properties for Multithreaded Shared-Memory Programs
+</li>
+
+<li>
+19:00 - 19:10:
+Deciding ω-Regular Properties on Linear Recurrence Sequences
+</li>
+
+<li>
+19:10 - 19:20:
+Deciding Reachability under Persistent x86-TSO
+</li>
+
+<li>
+19:20 - 19:30:
+On the Complexity of Bidirected Interleaved Dyck-Reachability
+</li>
+
+</ul>
+
+
+
+18:30 - 19:30: <b>Types and Functional Languages</b>
+{" "} in {" "}
+<a href="https://zoom.us/j/94721542929?pwd=dlhVczRWeTcwWE56dTlCOHFLM09CQT09">POPL-B</a>
+
+<ul>
+
+<li>
+18:30 - 18:40:
+Diamonds Are Not Forever: Liveness in Reactive Programming with Guarded Recursion
+</li>
+
+<li>
+18:40 - 18:50:
+On the Semantic Expressiveness of Recursive Types
+</li>
+
+<li>
+18:50 - 19:00:
+Automatic Differentiation in PCF
+</li>
+
+<li>
+19:00 - 19:10:
+Intersection Types and (Positive) Almost-Sure Termination
+</li>
+
+<li>
+19:10 - 19:20:
+Intensional Datatype Refinement
+</li>
+
+<li>
+19:20 - 19:30:
+Abstracting Gradual Typing Moving Forward : Precise and Space-Efficient
+</li>
+
+</ul>
+
+
+
+15:45 - 16:00: <b>Business Meeting and Town Hall</b>
+{" "} in {" "}
+<a href="https://acm-org.zoom.us/j/91683264924?pwd=bktYNUZ6Y3BFWVNXczM4OGhmSVBjdz09">Plenary room</a>
+
+
+
+          </p>
+        </Container>
+    </>
+  )
+}
