43d2361f24ae3c6a31a9536be1252970e0ceca21
Ross Gardiner <github@public.rossng.eu>
2021-03-25 16:14:36 +0000

    Add initial blog and TinaCMS skeleton

---
+++ gatsby-browser.js
@@ -0,0 +1,18 @@
+const React = require("react");
+const TinaCMS = require("tinacms");
+
+exports.wrapRootElement = ({ element }) => {
+  window.tinacms = new TinaCMS.TinaCMS({
+    enabled: undefined,
+    sidebar: {
+      hidden: process.env.NODE_ENV === "production",
+      position: "displace",
+    },
+    toolbar: undefined,
+  });
+  return React.createElement(
+    TinaCMS.TinaProvider,
+    { cms: window.tinacms },
+    element
+  );
+};
