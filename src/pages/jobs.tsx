5f491ece5fcbf0597f20fa1a06086bac293d53e5
Benjamin Pierce <bcpierce00t@gmail.com>
2021-07-17 20:55:19 -0400

    Finished pass on jobs page

--- src/pages/jobs.tsx
+++ src/pages/jobs.tsx
@@ -46,7 +46,9 @@ export default function About() {
               with a particular interest in front-end design and implementation.
               You should have an eye for what looks good and an instinct for good
               user experience.
-              {" "}
+              </Text>
+
+              <Text>
               <b>Essential skills: </b>
               Front-end web application development,
               TypeScript,
@@ -72,7 +74,7 @@ export default function About() {
             spacing={6}
           >
             <Heading as="h4" size="lg" p={0} mt={0}>
-              Open position: Software developer (devops / backend)
+              Open position: DevOps / back-end developer
             </Heading>
 
             <Text>
@@ -84,7 +86,9 @@ export default function About() {
               Ideally, you would have significant experience working on
               cloud-hosted applications; even better if at serious
               scale.
-            {" "}
+              </Text>
+
+              <Text>
             <b>Essential skills: </b>
               Web application development,
               Typescript,
@@ -112,72 +116,12 @@ export default function About() {
               Open position: UX / UI designer
             </Heading>
 
-            <Text>
-              Our ideal candidate for this position is a designer with 2 to 4
-              years of experience, with an interest beyond just the UX/UI design
-              reaching into front-end implementation. You should have an eye for
-              what looks good and an instinct for good user experience.
-            </Text>
-
-            <Flex w="100%" flexWrap="wrap">
-              <VStack
-                flexGrow="0"
-                flexShrink="0"
-                flexBasis={["100%", "50%"]}
-                px={[0, 2]}
-              >
-                <Heading as="h5" fontSize="lg">
-                  Essential skills
-                </Heading>
-                <Divider borderColor="gray.400" w="80px" />
-                <UnorderedList>
-                  <ListItem>User experience design (UX)</ListItem>
-                  <ListItem>User interface design (UI)</ListItem>
-                  <ListItem>User studies / research</ListItem>
-                  <ListItem>Accessible design</ListItem>
-                  <ListItem>React-ready design</ListItem>
-                </UnorderedList>
-              </VStack>
-
-              <VStack
-                flexGrow="0"
-                flexShrink="0"
-                flexBasis={["100%", "50%"]}
-                px={[0, 2]}
-              >
-                <Heading as="h5" fontSize="lg">
-                  Nice to have
-                </Heading>
-                <Divider borderColor="gray.400" w="80px" />
-                <UnorderedList>
-                  <ListItem>Chakra UI</ListItem>
-                  <ListItem>Front-end development</ListItem>
-                  <ListItem>TypeScript</ListItem>
-                  <ListItem>React</ListItem>
-                  <ListItem>GraphQL</ListItem>
-                </UnorderedList>
-              </VStack>
-            </Flex>
-
-            <VStack alignItems="flex-start">
-              <Heading as="h5" fontSize="lg">
-                Goals
-              </Heading>
-
               <Text>
-                Clowdr is a large and complex progressive-web-app. Your main
-                task will be to tame that complexity into an easy-to-use
+                Clowdr is a large and complex Progressive Web App. Your main
+                task will be to help us tame that complexity into an easy-to-use
                 experience for the wide variety of people that attend
                 conferences on our platform.
-              </Text>
-            </VStack>
 
-            <VStack alignItems="flex-start">
-              <Heading as="h5" fontSize="lg">
-                Responsibilities
-              </Heading>
-
-              <Text>
                 You will be responsible for the UX/UI design process (and if you
                 are interested in helping with the frontend implementation,
                 that's cool too!) We expect you to undertake the creation of
@@ -186,8 +130,35 @@ export default function About() {
                 our community) and translate those user experiences into a
                 modern, sleek UI design.
               </Text>
-            </VStack>
+
+            <Text>
+              Our ideal candidate for this position is a designer with 2 to 4
+              years of experience, with an interest beyond just the UX/UI design
+              reaching into front-end implementation. You should have an eye for
+              what looks good and an instinct for good user experience.
+            </Text>
+
+              <Text>
+            <b>Essential skills: </b>
+              User experience design (UX),
+              User interface design (UI),
+              User studies / research,
+              Accessible design,
+              React-ready design.
+            {" "}
+            <b>Nice to have: </b>
+              Chakra UI,
+              Front-end development,
+              TypeScript,
+              React,
+              GraphQL.
+            </Text>
           </VStack>
+
+          <Heading as="h2" size="2xl" textAlign="center" w="100%">
+            Working at Clowdr
+          </Heading>
+
 
           <VStack
             bg="gray.50"
@@ -204,12 +175,12 @@ export default function About() {
               </Heading>
 
               <Text>
-                You should be friendly and easy to work with, and comfortable
+                You should be friendly, easy to work with, and comfortable
                 working remotely with an internationally distributed team. We
                 encourage flexible working and asynchronous collaboration, but
                 the international nature of our business means meetings can be
-                at a wide range of hours. Our existing engineering team work
-                from European timezones.
+                at a wide range of hours. Our existing engineering team works
+                in European timezones.
               </Text>
 
               <Text>
@@ -221,12 +192,6 @@ export default function About() {
                 descriptions above.
               </Text>
 
-              <Text>
-                The ideal candidate for any position will be comfortable
-                pitching in ideas or code at all levels of our stack when
-                necessary. We develop all of our software{" "}
-                <a href="https://github.com/clowdr-app/clowdr">in the open</a>.
-              </Text>
             </VStack>
 
             <Divider borderColor="gray.300" w="80%" alignSelf="center" />
@@ -236,11 +201,12 @@ export default function About() {
                 What we offer
               </Heading>
 
-              <Text>
-                A critical role in a public-interest, community-focused company
-                that is developing cutting-edge, open-source technology for a
-                hot market sector, driven by a passion to combat climate change
-                while increasing inclusivity at conferences.
+              <Text> Working at Clowdr is an opportunity to play a
+                critical role in a public-interest, community-focused
+                company that is developing cutting-edge, open-source
+                technology for a hot market sector, driven by a
+                passion to combat climate change while increasing
+                inclusiveness and accessibility of virtual conferences.
               </Text>
 
               <Text>
@@ -250,13 +216,18 @@ export default function About() {
                 quite like the feeling of 2,000 conference attendees expressing
                 appreciation for your product at the end of a 3 week conference!
               </Text>
+
+              <Text>
+                We develop all of our software{" "}
+                <a href="https://github.com/clowdr-app/clowdr">in the open</a>.
+              </Text>
             </VStack>
 
             <Divider borderColor="gray.300" w="80%" alignSelf="center" />
 
             <VStack alignItems="flex-start">
               <Heading as="h3" fontSize="xl" p={0} mt={0}>
-                The Nitty-gritty 
+                The nitty-gritty 
               </Heading>
 
             <SimpleGrid
@@ -315,38 +286,6 @@ export default function About() {
                 resident or have the right to work in the UK for this role. We
                 are not offering visa sponsorship at this time.
               </Text>
-
-          </VStack>
-
-          <VStack
-            alignItems="flex-start"
-            bg="gray.50"
-            shadow="md"
-            rounded="lg"
-            my={8}
-            p={8}
-          >
-            <Heading as="h3" fontSize="xl" p={0} mt={0}>
-              The company
-            </Heading>
-
-            <Text>
-              Clowdr is a young and quickly growing conference platform that
-              aims to reduce the carbon footprint of academic conferences by
-              offering a more attractive virtual alternative.
-            </Text>
-
-            <Text>
-              Clowdr CIC has 5 founders, all contributing to every aspect of the
-              business. Ed and Ross are full time CEO and CTO. Both are software
-              developers by background, though Ed also has entrepreneurship
-              training. Benjamin, Jonathan, and Crista remain full-time in their
-              academic positions and contribute as much as possible to Clowdr.
-              Ed and Ross currently do the bulk of technical development, and
-              the whole team contributes to business development and operations.
-              We have recently been joined by a new business development manager
-              to focus on scaling up the latter activities.
-            </Text>
           </VStack>
 
           <VStack
@@ -371,14 +310,12 @@ export default function About() {
             <Button as="a" href="mailto:jobs@clowdr.org" alignSelf="center">
               Apply via jobs@clowdr.org
             </Button>
-          </VStack>
 
-          <Alert status="warning">
-            <AlertIcon />
-            <AlertDescription>
-              We are not looking for external recruitment partners at this time.
-            </AlertDescription>
-          </Alert>
+            <Text>
+                          (We are not looking to partner with external
+                          recruitment agencies at this time.)
+            </Text>
+          </VStack>
         </Container>
       </Layout>
     </>
