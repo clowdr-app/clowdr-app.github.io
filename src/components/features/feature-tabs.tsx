1bab2ee82eef1ced80e23b84c0ecf21f0d11d52d
Ed Nutting <1746336+EdNutting@users.noreply.github.com>
2022-05-22 02:36:54 +0100

    Remove pointless fade effect and try to avoid SSR issues :/

--- src/components/features/feature-tabs.tsx
+++ src/components/features/feature-tabs.tsx
@@ -1,7 +1,6 @@
 import type { TabsProps } from "@chakra-ui/react";
-import { HStack, SlideFade, Tabs, VStack } from "@chakra-ui/react";
-import React, { useState } from "react";
-import VisibilitySensor from "react-visibility-sensor";
+import { HStack, Tabs, VStack } from "@chakra-ui/react";
+import React from "react";
 import { useIsNarrow } from "../../hooks/useIsNarrow";
 
 export function FeatureTabs({
@@ -9,36 +8,16 @@ export function FeatureTabs({
   ...props
 }: React.PropsWithChildren<TabsProps>): JSX.Element {
   const isNarrow = useIsNarrow();
-  const [isVisible, setIsVisible] = useState<boolean>(false);
   return (
-    <VisibilitySensor
-      offset={{ bottom: 200 }}
-      partialVisibility={true}
-      onChange={(iv: boolean) => {
-        if (iv) {
-          setIsVisible(true);
-        }
-      }}
+    <Tabs
+      as={isNarrow ? VStack : HStack}
+      {...props}
+      spacing={8}
+      alignItems={isNarrow ? "center" : "flex-start"}
+      justifyContent="flex-start"
+      display="flex"
     >
-      <SlideFade
-        in={isVisible}
-        transition={{
-          enter: {
-            duration: 0.5,
-          },
-        }}
-      >
-        <Tabs
-          as={isNarrow ? VStack : HStack}
-          {...props}
-          spacing={8}
-          alignItems={isNarrow ? "center" : "flex-start"}
-          justifyContent="flex-start"
-          display="flex"
-        >
-          {children}
-        </Tabs>
-      </SlideFade>
-    </VisibilitySensor>
+      {children}
+    </Tabs>
   );
 }
--- src/hooks/useIsNarrow.tsx
+++ src/hooks/useIsNarrow.tsx
@@ -1,7 +1,3 @@
-import { useBreakpoint } from "@chakra-ui/react";
-
 export function useIsNarrow(): boolean {
-  const bp = useBreakpoint("lg");
-  const isNarrow = bp === "base" || bp === "sm" || bp === "md";
-  return isNarrow;
+  return window.innerWidth < 500;
 }
